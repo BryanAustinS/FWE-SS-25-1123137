7edf74155b786d990a1fa9aee54a162d4e928aef
Bryan Austin Suharta <66191560+BryanAustinS@users.noreply.github.com>
2025-04-19 17:38:04 +0200

    Added Unsplash Service to DI

--- src/dependency-injection.ts
+++ src/dependency-injection.ts
@@ -10,6 +10,7 @@ import { TripRepository } from './database/repository/trip.repository';
 import { DestinationRepository } from './database/repository/destination.repository';
 import { TripController } from './controller/trip.controller';
 import { DestinationController } from './controller/destination.controller';
+import { UnsplashService } from './unsplash.service';
 
 export const DI = {} as {
   app: App;
@@ -24,6 +25,9 @@ export const DI = {} as {
     trip: TripController;
     destination: DestinationController;
   };
+  services: {
+    unsplash: UnsplashService
+  }
 };
 
 export function initializeDI() {
@@ -36,6 +40,10 @@ export function initializeDI() {
     destination:
       new DestinationRepository(DI.db),
   };
+
+  DI.services = {
+    unsplash: new UnsplashService(),
+  }
 
   // Initialize Controller
   DI.controller = {
